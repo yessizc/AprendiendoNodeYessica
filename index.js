1ac0a6fd00e9540801a3c4a32160ec7147cac9ba
yessica <yazabala38@misena.edu.co>
2022-08-19 20:45:49 -0500

    clase 4

--- index.js
+++ index.js
@@ -3,6 +3,49 @@
 servidor.iniciar()*/
 
 const express = require('express');
-const app = express(); //constructor 
+const app = express();
+//const calc = require('./MiCalculadora')
+const path = require('path');
+const PORT = process.env.PORT || 9000;
+const { dirname } = require('path');
 
-app.listen(7575);
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+
+app.get('/', function(req, res) {
+    //console.log(path.__dirname);
+    //var nombre = "Usuario"
+    //es.render('pages/index', { name: nombre })
+    let user = {
+        nombre: "Alvaro",
+        rol: "Vendedor",
+        Puesto: "Tienda"
+    };
+    let compra = ["carro", "moto", "bicicleta"]
+    let nombre = "usuario"
+    res.render('pages/index', {
+        name: nombre,
+        usuario: user,
+        carrito: compra
+    });
+
+
+
+})
+
+
+app.listen(PORT);
+console.log('Servidor iniciado');
+
+
+
+
+
+
+
+
+//app.set('views', path.join);
